369d99e7d5317043d91db2979e84ad09fd518519
Ray Yu <kaito-akira@hotmail.com>
2022-12-23 12:19:32 -0800

    create pattern

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
-alert("Hello! I am an alert box!!");
+
+let gamePattern = [];
+
+let buttonColours = ['red', 'blue', 'green', 'yellow'];
+
+function nextSequence () {
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColours = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColours);
+}
+
+nextSequence();
+
+
+
+
+console.log(gamePattern)
