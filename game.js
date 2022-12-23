226c3acc25501e68f0ea2d15c77f42e49993666c
Ray Yu <kaito-akira@hotmail.com>
2022-12-23 12:26:47 -0800

    add animation, sound to button

--- game.js
+++ game.js
@@ -7,11 +7,10 @@ function nextSequence () {
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColours = buttonColours[randomNumber];
   gamePattern.push(randomChosenColours);
+
+  $("#" + randomChosenColours).fadeIn(100).fadeOut(100).fadeIn(100);
+  let audio = new Audio('sounds/'+ randomChosenColours + ".mp3");
+  audio.play();
 }
-
-nextSequence();
-
-
-
 
 console.log(gamePattern)
